4e6b099783751bb7ee8178f685676e7031ea0b33
sernoomze <zaanoom@gmail.com>
2019-07-12 10:17:09 +0700

    Update OcsSerNOoMzE.sh

--- OcsSerNOoMzE.sh
+++ OcsSerNOoMzE.sh
@@ -71,7 +71,7 @@ read -p "ใส่ไฟล์ sernoomze.zip ใน /home/vps/public_html กด
 
 apt-get -y install zip unzip
 cd /home/vps/public_html
-wget https://github.com/sernoomze/Ocs-web/blob/master/deathside_ocs.zip
+wget https://raw.githubusercontent.com/sernoomze/Ocs-web/master/deathside_ocs.zip
 unzip deathside_ocs.zip
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
