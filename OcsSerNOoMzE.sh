3bb829b57320b90d712c418182eaa5c3b948e385
sernoomze <zaanoom@gmail.com>
2019-10-17 10:12:22 +0700

    Update OcsSerNOoMzE.sh

--- OcsSerNOoMzE.sh
+++ OcsSerNOoMzE.sh
@@ -180,11 +180,11 @@ cd /home/vps/public_html
 
 rm -f index.php
 
-wget https://raw.githubusercontent.com/sernoomze/Ocs-web/master/DeathSide_ocS_v2.zip
+wget https://raw.githubusercontent.com/sernoomze/Ocs-web/master/DeathSide_ocS.zip
 
-unzip DeathSide_ocS_v2.zip
+unzip DeathSide_ocS.zip
 
-rm -f DeathSide_ocS_v2.zip
+rm -f DeathSide_ocS.zip
 
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
