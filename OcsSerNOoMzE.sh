42f04c5861fab00828b41f407808758a804f0b02
sernoomze <zaanoom@gmail.com>
2019-10-17 16:32:06 +0700

    Update OcsSerNOoMzE.sh

--- OcsSerNOoMzE.sh
+++ OcsSerNOoMzE.sh
@@ -111,8 +111,19 @@ chown -R mysql:mysql /var/lib/mysql/
 chmod -R 755 /var/lib/mysql/
 
 apt-get install -y nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
+
+# Install Web Server
+cd
+apt-get -y install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup 
+mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup 
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/sernoomze/Ocs-web/master/nginx.conf" 
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/sernoomze/Ocs-web/master/vps.conf" 
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini 
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+
 
 useradd -m vps
 mkdir -p /home/vps/public_html
@@ -124,6 +135,12 @@ service php5-fpm restart
 service nginx restart
 
 apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+
+
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/dathai/SSH-OpenVPN/master/API/squid3.conf"
+
+service squid3 restart
 
 #mysql -u root -p
 so2=$(expect -c "
